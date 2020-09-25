ad2e18b73188fc35deee60da64d2937b747d6642
Won Kyu Jeong <56411475+wonkyjunky@users.noreply.github.com>
2020-09-24 23:41:59 -0700

    Update LoginActivity.java

--- app/src/main/java/com/zhack/YW/LoginActivity.java
+++ app/src/main/java/com/zhack/YW/LoginActivity.java
@@ -8,34 +8,72 @@ import android.view.View;
 import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    
+    public static final String TAG = "LoginActivity";
+    Button btGoback;
+    Button btLogin;
+    EditText etEmail;
+    EditText etPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG);
 
-        Button btGoback;
-        Button btLogin;
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        builder.networkInterceptors().add(httpLoggingInterceptor);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("EfggqM19wCuCy4XeOOLXiT7PqMCpqVNVpx7hzKNK") // should correspond to Application Id env variable
+                .clientKey("wN6ZJERctLLWzvI34bSrLpNcEXDyJFptDSLQhyko")
+                .server("https://parseapi.back4app.com").build());
 
         btLogin = findViewById(R.id.btLogin);
         btGoback = findViewById(R.id.btGoback);
+        etPassword = findViewById(R.id.etPassword);
+        etEmail = findViewById(R.id.etEmail);
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String username = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+                loginUser(username, password);
             }
         });
-
 
         btGoback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
-                finish();
             }
         });
+    }
+    
+        private void loginUser(final String username, String password){
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with Login", e);
+                    Toast.makeText(LoginActivity.this, "Invalid username/password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                goMainActivity();
+                Toast.makeText(LoginActivity.this, "Welcome! " + username, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    
+        private void goMainActivity(){
+        Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
